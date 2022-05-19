331833345a45efe312288f6e96fbd8fdc6dacb8f
kolomuec <100859387+kolomuec@users.noreply.github.com>
2022-05-19 16:01:31 +0300

    Add files via upload

---
+++ TaskWithVariables.java
@@ -0,0 +1,15 @@
+package com.max.idea;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class TaskWithVariables {
+    public static void main(String[] args) {
+        // Создайте переменную типа с плавающей точкой с наибольшим диапазоном чисел.
+        // Присвойте ей произвольное дробное значение
+        double dbl = 67.98787;
+
+        //Создайте переменную целочисленного типа с наименьшим диапазоном чисел.
+        // Присвойте ей наибольшее возможное значение
+        byte bt = 4;
+    }
+}
